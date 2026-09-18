0cfe9032984d5333b73b453a2882b9f5271a7070
Ashna Tony <161132293+ashnatony@users.noreply.github.com>
2026-09-18 10:57:38 +0530

    Update model file name in app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-pipeline = joblib.load("polynomial_regression_ac_fan_model.pkl")
+pipeline = joblib.load("electric_bill_model(1).pkl")
 
 poly = pipeline["poly"]
 model = pipeline["model"]
